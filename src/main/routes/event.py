96cbdfb90971f20c3a02ec1b731bddb74dcddbbd
arthurvignati <arthurvig8@gmail.com>
2025-08-07 17:14:12 -0300

    Feat: Criação das primeiras rotas de eventos

---
+++ src/main/routes/event.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, jsonify
+
+#criar um agregador de rotas
+#todas as rotas relacionadas a eventos estarão aqui
+#EX: para criar uma rota /event, basta chamar o agregador @event_route_bp.route("/event", method=["POST"])
+event_route_bp = Blueprint("event_route", __name__)
+
+
+@event_route_bp.route("/event", method=["POST"])
+def create_new_event():
+    return jsonify({ "estou": "aqui" }), 201
--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from src.main.routes.event import event_route_bp
 
 app = Flask(__name__) #criar servidor http Flask
+
+app.register_blueprint(event_route_bp) #registrar o agregador de rotas do evento no servidor. 
+#                                       Qualquer rota que seja criada no agregador, será cadastrada no servidor
 
 
 
