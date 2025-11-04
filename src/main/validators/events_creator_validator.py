e8a6ea0cfe4a73a81bc3f0bbf7820eacb83365cf
arthurvignati <arthurvig8@gmail.com>
2025-11-04 17:58:23 -0300

    refactor: validator body event

--- src/main/validators/events_creator_validator.py
+++
@@ -1,9 +0,0 @@
-#validação do body
-from cerberus import Validator
-
-def events_creator_validator(request: any):
-    Validator({
-        "data":{
-            "type": "dict"
-        }
-    })
---
+++ src/validators/events_creator_validator.py
@@ -0,0 +1,26 @@
+#validação do body
+from cerberus import Validator
+
+def events_creator_validator(request: any):
+    body_validator = Validator({
+        "data":{
+            "type": "dict",
+            "schema": {
+                "name": { "type": "string", "required": True, "empty": False}
+            }
+        }
+    })
+
+    response = body_validator.validate(request.json) #validando o body do flask
+
+    if response is False:
+        print(body_validator.errors)
+
+
+# Example:
+# {
+#     "data": {
+#         "name": "Event1"
+#     }
+
+# }
