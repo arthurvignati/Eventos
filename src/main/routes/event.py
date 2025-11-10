ba2ff2d74194b762429a3681ad11c4fa7b9fe3ae
arthurvignati <arthurvig8@gmail.com>
2025-11-10 13:32:19 -0300

    feat: implementing subscribers validator

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from src.main.http_types.http_response import HttpResponse
 from src.main.http_types.http_request import HttpRequest
-from src.validators.events_creator_validator import events_creator_validator
+from src.validators.events_creator_validator import events_creator_validator, subscribers_creator_validator
 
 #criar um agregador de rotas
 #todas as rotas relacionadas a eventos estarão aqui
@@ -11,6 +11,7 @@ event_route_bp = Blueprint("event_route", __name__)
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
     events_creator_validator(request)
+    subscribers_creator_validator(request)
     http_request = HttpRequest(body=request.json) #Conseguimos ver aqui todos os "atributos" de um request que estão na padronização 
     print(http_request.body)
 
